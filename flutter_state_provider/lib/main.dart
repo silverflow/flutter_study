422aa46c886fa75adaf46926ac1457e9ff5eebb3
silverflow <silveryflow@naver.com>
2022-10-29 13:32:04 +0900

    provider example

--- flutter_state_provider/lib/main.dart
+++ flutter_state_provider/lib/main.dart
@@ -1,30 +1,38 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_state_provider/models/cart.dart';
+import 'package:flutter_state_provider/repositories/item_list.dart';
+import 'package:flutter_state_provider/screens/screen_item.dart';
+import 'package:flutter_state_provider/screens/screen_cart.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+        Provider(
+          create: (_) => ItemList(),
+        )
+      ],
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Flutter Provider Example',
+          initialRoute: '/',
+          routes: {
+            '/': (context) => ItemScreen(),
+            '/cart': (context) => CartScreen(),
+          },
+        );
+      },
     );
   }
 }
