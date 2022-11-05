9007f488d6933f034b41d05edb066a60800bc2f3
silverflow <silveryflow@naver.com>
2022-11-05 23:51:23 +0900

    gitignore 추가 및 회원가입 화면

---
+++ flutter_shopping_mall/lib/models/model_auth.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+enum AuthStatus {
+  registerSuccess,
+  registerFail,
+  loginSuccess,
+  loginFail,
+}
+
+class FirebaseAuthProvider with ChangeNotifier {
+  FirebaseAuth authClient;
+  User? user;
+
+  FirebaseAuthProvider({auth}) : authClient = auth ?? FirebaseAuth.instance;
+
+  Future<AuthStatus> registerWithEmail(String email, String password) async {
+    try {
+      UserCredential credential = await authClient
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return AuthStatus.registerSuccess;
+    } catch (e) {
+      print(e);
+      return AuthStatus.registerFail;
+    }
+  }
+}
