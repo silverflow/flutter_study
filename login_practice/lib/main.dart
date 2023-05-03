87d45bbb9c7d83e37f4fb5c73b3dc6540e8b5a4e
silverflow <silveryflow@naver.com>
2023-05-04 01:27:53 +0900

    코딩셰프 조금 매운맛 로그인 리팩토링

---
+++ login_practice/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:login_practice/login_app/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase login app',
+      home: LogIn(),
+    );
+  }
+}
