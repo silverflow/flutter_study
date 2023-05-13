6d2621c4b9d693aa5743c38196fbe617a613b35b
silverflow <silveryflow@naver.com>
2023-05-14 02:35:10 +0900

    weather app init

---
+++ weather_app/lib/screens/loading.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
+  void getLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch (e) {
+      print('There was a problem with the internet connection.');
+    }
+  }
+
+  void fetchData() async {
+    http.Response response = await http.get(Uri.parse(
+        "https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1"));
+    print(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Get my location'),
+          onPressed: null,
+        ),
+      ),
+    );
+  }
+}
